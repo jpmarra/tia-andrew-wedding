5e4c507a3f7c2522983e63796feb506230e8f789
jmarra <jp.marra@mlb.com>
2019-07-19 14:28:11 -0400

    add not supported html

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,7 +8,13 @@ export const onClientEntry = () => {
     if (browser) {
         if (browser.name === 'ie') {
             const gatsbyEl = document.getElementById('___gatsby')
-            gatsbyEl.innerHTML = 'Browser is not supported'
+            const supportDiv = document.createElement('div')
+            const text = document.createTextNode(
+                'This browser is not supported. Please visit this website in a modern browser for the best experience.'
+            )
+            supportDiv.appendChild(text)
+            supportDiv.classList.add('not-supported')
+            gatsbyEl.appendChild(supportDiv)
         }
     }
 }
