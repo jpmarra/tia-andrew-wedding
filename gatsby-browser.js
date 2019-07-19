7c07db23e0ece4f5941b97f62f595db486c23a0f
jmarra <jp.marra@mlb.com>
2019-07-19 14:56:07 -0400

    switch hook

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import './src/styles/global.css'
 const { detect } = require('detect-browser')
 
-export const onInitialClientRender = () => {
+export const onClientEntry = () => {
     const browser = detect()
 
     // handle the case where we don't detect the browser
