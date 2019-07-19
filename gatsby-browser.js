6d0cb76d815ae8b8f45be4a5b2d707e1394eae6a
jmarra <jp.marra@mlb.com>
2019-07-19 14:58:34 -0400

    append instead of replace

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,18 @@
 import './src/styles/global.css'
 const { detect } = require('detect-browser')
 
-export const onClientEntry = () => {
+export const onInitialClientRender = () => {
     const browser = detect()
 
     // handle the case where we don't detect the browser
     if (browser) {
         if (browser.name === 'ie') {
-            const gatsbyEl = document.getElementById('gatsby-container')
-            console.log('element: ', gatsbyEl)
-            gatsbyEl.innerHTML =
-                '<div class="not-supported"><div>This browser is not supported. Please visit this website in a modern browser for the best experience.<div></div>'
+            const gatsbyEl = document.getElementById('site-container')
+            const supportDiv = document.createElement('div')
+            const text = document.createTextNode()
+            supportDiv.appendChild(text)
+            supportDiv.classList.add('not-supported')
+            gatsbyEl.appendChild(supportDiv)
         }
     }
 }
