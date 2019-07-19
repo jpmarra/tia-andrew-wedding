1f0c081cf9c84da989feb109cadcfb0b671ef4db
jmarra <jp.marra@mlb.com>
2019-07-19 14:31:07 -0400

    change gatsby hook

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,13 @@
 import './src/styles/global.css'
 const { detect } = require('detect-browser')
 
-export const onClientEntry = () => {
+export const onInitialClientRender = () => {
     const browser = detect()
 
     // handle the case where we don't detect the browser
     if (browser) {
         if (browser.name === 'ie') {
-            const gatsbyEl = document.getElementById('___gatsby')
+            const gatsbyEl = document.getElementById('site-container')
             const supportDiv = document.createElement('div')
             const text = document.createTextNode(
                 'This browser is not supported. Please visit this website in a modern browser for the best experience.'
