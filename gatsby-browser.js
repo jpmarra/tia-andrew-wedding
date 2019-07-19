8d6b750300b9abea7f31f9467d599cad835838e8
jmarra <jp.marra@mlb.com>
2019-07-19 14:47:28 -0400

    wrap inner div

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,7 +9,7 @@ export const onInitialClientRender = () => {
         if (browser.name === 'ie') {
             const gatsbyEl = document.getElementById('site-container')
             gatsbyEl.innerHTML =
-                '<div class="not-supported">This browser is not supported. Please visit this website in a modern browser for the best experience.</div>'
+                '<div class="not-supported"><div>This browser is not supported. Please visit this website in a modern browser for the best experience.<div></div>'
         }
     }
 }
