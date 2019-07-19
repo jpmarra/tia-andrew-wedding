84bc1ca07b118505bc4eb084e9355f315ca33980
jmarra <jp.marra@mlb.com>
2019-07-19 14:17:09 -0400

    fix type

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,7 @@ export const onClientEntry = () => {
 
     // handle the case where we don't detect the browser
     if (browser) {
-        if (browser.name === ie) {
+        if (browser.name === 'ie') {
             const gatsbyEl = document.getElementById('__gatsby')
             gatsbyEl.innerHTML = 'Browser is not supported'
         }
