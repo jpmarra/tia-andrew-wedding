444c1d9f27a94686ed89a0fdee9c1a90e9681918
jmarra <jp.marra@mlb.com>
2019-07-20 22:56:31 -0400

    create 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import Layout from '../components/layout'
+import Container from '../components/container'
+import Header from '../components/header'
+
+import QuestionList from '../components/questionlist'
+import Footer from '../components/footer'
+export default () => {
+    const Wrapper = styled.div`
+        width: 100%;
+        min-height: calc(100vh - 141px);
+        display: flex;
+        justify-content: center;
+        background-color: ${props => props.theme.cream};
+    `
+    return (
+        <Layout>
+            <Header />
+            <Wrapper>
+                <Container class="center">404</Container>
+            </Wrapper>
+            <Footer />
+        </Layout>
+    )
+}
